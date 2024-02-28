ffbf29cbd20cc351534ed6b2dd73ae4df4c7dd5b
lanlandeli <2218274624@qq.com>
2024-02-28 10:00:12 +0800

    vue修改

---
+++ vue/src/components/layouts/BaseHeader.vue
@@ -0,0 +1,45 @@
+<script lang="ts" setup>
+import { ref, defineEmits } from 'vue'
+import { toggleDark } from "~/composables"
+
+const activeIndex = ref('1')
+const emit = defineEmits(['update:index'])
+
+const handleSelect = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+  emit('update:index', key) // Emit the selected index to the parent
+}
+</script>
+
+<template>
+  <el-menu
+    :default-active="activeIndex"
+    class="el-menu-demo"
+    mode="horizontal"
+    @select="handleSelect"
+  >
+    <el-menu-item index="1">Processing Center</el-menu-item>
+    <el-sub-menu index="2">
+      <template #title>Workspace</template>
+      <el-menu-item index="2-1">item one</el-menu-item>
+      <el-menu-item index="2-2">item two</el-menu-item>
+      <el-menu-item index="2-3">item three</el-menu-item>
+      <el-sub-menu index="2-4">
+        <template #title>item four</template>
+        <el-menu-item index="2-4-1">item one</el-menu-item>
+        <el-menu-item index="2-4-2">item two</el-menu-item>
+        <el-menu-item index="2-4-3">item three</el-menu-item>
+      </el-sub-menu>
+    </el-sub-menu>
+    <el-menu-item index="3" disabled>Info</el-menu-item>
+    <el-menu-item index="4">Orders</el-menu-item>
+    <el-menu-item h="full" @click="toggleDark()">
+      <button
+        class="border-none w-full bg-transparent cursor-pointer"
+        style="height: var(--ep-menu-item-height)"
+      >
+        <i inline-flex i="dark:ep-moon ep-sunny" />
+      </button>
+    </el-menu-item>
+  </el-menu>
+</template>
