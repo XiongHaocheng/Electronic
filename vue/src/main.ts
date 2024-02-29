b856d060614bbcbd0ac8f5eee7fa49b5da4a00e8
“xionghaocheng” <2923637940@qq.com>
2024-02-29 09:25:53 +0800

    拉取

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -1,8 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-
 import router from "./router";
-
 // import "~/styles/element/index.scss";
 
 import ElementPlus from "element-plus";
