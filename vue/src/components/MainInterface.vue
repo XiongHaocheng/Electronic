be8e8443f7904c33f54a6b2e5013bae66f1579c3
KAI <3097141423@qq.com>
2024-03-09 14:21:03 +0800

    前端购买

--- vue/src/components/MainInterface.vue
+++ vue/src/components/MainInterface.vue
@@ -1,6 +1,12 @@
 <script setup>
 import { ref } from 'vue'
 import  NewView from './MainViews/NewView.vue'
+import Computer from './NewViews/Computer.vue'
+import Elephone from './NewViews/Elephone.vue'
+import Watch from './NewViews/Watch.vue'
+import Mouse from './NewViews/Mouse.vue'
+import Jianpan from './NewViews/Jianpan.vue'
+import Displayer from './NewViews/Displayer.vue'
 const props = defineProps(['index'])
 const awesome = ref(true)
 </script>
@@ -11,16 +17,22 @@ const awesome = ref(true)
  <NewView />
   </template>
   <template v-if="index=='2-1'">
-    2-1
+    <Computer></Computer>
   </template>
   <template v-if="index=='2-2'">
- <h1>界面2-2</h1>
+ <Elephone></Elephone>
   </template>
   <template v-if="index=='2-3'">
- <h1>界面2-3</h1>
+ <Watch></Watch>
   </template>
   <template v-if="index=='2-4-1'">
- <h1>界面2-4-1</h1>
+    <Mouse></Mouse>
+  </template>
+  <template v-if="index=='2-4-2'">
+    <Jianpan></Jianpan>
+  </template>
+  <template v-if="index=='2-4-3'">
+    <Displayer></Displayer>
   </template>
 </template>
 
--- vue/src/components/layouts/BaseHeader.vue
+++ vue/src/components/layouts/BaseHeader.vue
@@ -20,18 +20,18 @@ const handleSelect = (key: string, keyPath: string[]) => {
   >
     <el-menu-item index="1">新品发布</el-menu-item>
     <el-sub-menu index="2">
-      <template #title>Workspace</template>
-      <el-menu-item index="2-1">item one</el-menu-item>
-      <el-menu-item index="2-2">item two</el-menu-item>
-      <el-menu-item index="2-3">item three</el-menu-item>
+      <template #title>购买</template>
+      <el-menu-item index="2-1">电脑</el-menu-item>
+      <el-menu-item index="2-2">手机</el-menu-item>
+      <el-menu-item index="2-3">智能穿戴</el-menu-item>
       <el-sub-menu index="2-4">
-        <template #title>item four</template>
-        <el-menu-item index="2-4-1">item one</el-menu-item>
-        <el-menu-item index="2-4-2">item two</el-menu-item>
-        <el-menu-item index="2-4-3">item three</el-menu-item>
+        <template #title>外设</template>
+        <el-menu-item index="2-4-1">鼠标</el-menu-item>
+        <el-menu-item index="2-4-2">键盘</el-menu-item>
+        <el-menu-item index="2-4-3">显示器</el-menu-item>
       </el-sub-menu>
     </el-sub-menu>
-    <el-menu-item index="3" disabled>Info</el-menu-item>
+    <el-menu-item index="3" >Info</el-menu-item>
     <el-menu-item index="4">Orders</el-menu-item>
     <el-menu-item h="full" @click="toggleDark()">
       <button
